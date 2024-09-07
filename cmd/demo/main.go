e01c7bb5dd838e9fd7553751d12c9194d2bddac4
davidwmartines <d5172@yahoo.com>
2024-09-07 16:17:46 -0500

    Implement basic accounting.

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/davidwmartines/accountsim/internal/accounts"
+)
 
 func main() {
 	fmt.Println("demo!")
+
+	cash_account := accounts.Account{Name: "cash"}
+
+	income_source_account := accounts.Account{Name: "income source"}
+
+	all_transactions := make([]accounts.Transaction, 0, 20)
+
+	print_balance(&cash_account, all_transactions)
+	print_balance(&income_source_account, all_transactions)
+
+	trans1 := accounts.Transaction{
+		Entries: []accounts.Entry{
+			{Account: &income_source_account, Amount: -100},
+			{Account: &cash_account, Amount: 100}}}
+
+	all_transactions = append(all_transactions, trans1)
+
+	trans2 := accounts.Transaction{
+		Entries: []accounts.Entry{
+			{Account: &income_source_account, Amount: -250},
+			{Account: &cash_account, Amount: 250}}}
+
+	all_transactions = append(all_transactions, trans2)
+
+	print_balance(&cash_account, all_transactions)
+	print_balance(&income_source_account, all_transactions)
+
+}
+
+func print_balance(act *accounts.Account, all_transactions []accounts.Transaction) {
+	bal := act.Balance(all_transactions)
+	fmt.Printf("Account: %s balance: %d\n", act.Name, bal)
 }
--- internal/accounts/accounts.go
+++ internal/accounts/accounts.go
@@ -1,1 +1,30 @@
 package accounts
+
+type Account struct {
+	Name string
+}
+
+func (act *Account) Balance(source_transactions []Transaction) int {
+	var bal int
+	entries := make([]Entry, 0, 20)
+	for _, t := range source_transactions {
+		for _, e := range t.Entries {
+			if e.Account == act {
+				entries = append(entries, e)
+			}
+		}
+	}
+	for _, entry := range entries {
+		bal += entry.Amount
+	}
+	return bal
+}
+
+type Transaction struct {
+	Entries []Entry
+}
+
+type Entry struct {
+	Account *Account
+	Amount  int
+}
